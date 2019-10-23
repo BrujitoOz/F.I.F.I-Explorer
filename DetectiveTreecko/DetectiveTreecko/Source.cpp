70d74774348eee421213d77e643a17e6de7c9ff8
wilfredo morales <wmorales83@gmail.com>
2019-10-23 15:59:06 -0500

    update files

--- DetectiveTreecko/DetectiveTreecko/Source.cpp
+++ DetectiveTreecko/DetectiveTreecko/Source.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-//#include <dirent.h>
+#include <filesystem> // investigar. tiene funciones que permite iterar carpetas y archivos, extraer el tamanio de un archivo, etc
+
+using namespace std;
+using namespace std::experimental::filesystem; // libreria propuestra por c++, aun experimental
 
 int main() {
+	string path = "c:\\"; // la ruta que queremos escanear
+	for (const auto & entry : directory_iterator(path)) {
+		cout << entry.path() << endl;
+	}
 
+	system("pause");
 	return 0;
 }
