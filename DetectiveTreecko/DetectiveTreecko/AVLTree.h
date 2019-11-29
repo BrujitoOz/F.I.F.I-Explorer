d26ec45bd23efeb511f1eb1515ece931354d0ca7
Hernán Razo <ozemblem66@gmail.com>
2019-11-29 12:20:05 -0500

    Update AVLTree.h

--- DetectiveTreecko/DetectiveTreecko/AVLTree.h
+++ DetectiveTreecko/DetectiveTreecko/AVLTree.h
@@ -2,44 +2,48 @@
 #include <functional>
 #include <vector>
 #include <string>
-#define max(a,b) (a > b ? a : b)  
+#include <cstdint>
+#include <stdint.h>
+#define max(a,b) (a > b ? a : b) 
 using namespace std;
+typedef unsigned int uint;
+typedef long long int li;
+template<typename T>
+struct Node {
+	T Elem;
+	li h;
+	li n;
+	Node<T>* left;
+	Node<T>* right;
+	Node(T Elem) : Elem(Elem), left(nullptr), right(nullptr), h(0), n(1) {}
+};
 template<typename T, typename Comparable = T, T NONE = 0>
-class AVLTree { // Arbol balanceado que nos dio lord Canaval, lo unico nuevo por ahora es la funcion StartWith
-	struct Node {
-		T Elem;
-		int h; // altura
-		int n; // cantidad de elementos del nodo
-		Node* left;
-		Node* right;
-		Node(T Elem) : Elem(Elem), left(nullptr), right(nullptr), h(0), n(1) {}
-	};
-	Node* root;
-	int len;
-	function<Comparable(T)> Key; // recibe un alumno y retorna el nombre, recibe un archivo y retorna fecha de creacion / extension / tamanio del archivo, o sea retorna un elemento comparable
-	void destroy(Node* node) { // para destruir el arbol se hace en postorden
+class AVLTree {
+	Node<T>* root;
+	function<Comparable(T)> Key; 
+	void Destroy(Node<T>* node) {
 		if (node != nullptr) {
-			destroy(node->left);
-			destroy(node->right);
+			Destroy(node->left);
+			Destroy(node->right);
 			delete node;
 		}
 	}
-	Node* add(Node* node, T Elem) {
+	Node<T>* Add(Node<T>* node, T Elem) {
 		if (node == nullptr) {
-			node = new Node(Elem);
+			node = new Node<T>(Elem);
 		}
 		else {
 			if (Key(Elem) < Key(node->Elem)) {
-				node->left = add(node->left, Elem);
+				node->left = Add(node->left, Elem);
 			}
 			else {
-				node->right = add(node->right, Elem);
+				node->right = Add(node->right, Elem);
 			}
 			node = Balance(node);
 		}
 		return node;
 	}
-	T find(Node* node, Comparable val) {
+	T Find(Node<T>* node, Comparable val) {
 		if (node == nullptr) {
 			return NONE;
 		}
@@ -47,14 +51,13 @@ class AVLTree { // Arbol balanceado que nos dio lord Canaval, lo unico nuevo por
 			return node->Elem;
 		}
 		else if (val < Key(node->Elem)) {
-			return find(node->left, val);
+			return Find(node->left, val);
 		}
 		else {
-			return find(node->right, val);
+			return Find(node->right, val);
 		}
 	}
-	// esta es una funcion para encontrar una lista de coincidencias
-	void findStarWith(Node* node, Comparable val, function<bool(T, string)> op, vector<T> *list) {
+	void findStarWith(Node<T>* node, Comparable val, function<bool(T, string)> op, vector<T> *list) {
 		if (node == nullptr) {
 			return;
 		}
@@ -70,7 +73,7 @@ class AVLTree { // Arbol balanceado que nos dio lord Canaval, lo unico nuevo por
 			findStarWith(node->right, val, op, list);
 		}
 	}
-	void findContain(Node* node, Comparable val, function<bool(T, string)> op, vector<T>* list) {
+	void findContain(Node<T>* node, Comparable val, function<bool(T, string)> op, vector<T>* list) {
 		if (node == nullptr) {
 			return;
 		}
@@ -80,7 +83,7 @@ class AVLTree { // Arbol balanceado que nos dio lord Canaval, lo unico nuevo por
 		findContain(node->left, val, op, list);
 		findContain(node->right, val, op, list);
 	}
-	void Comp(Node* node, Comparable val, function<bool(T, long)> op, vector<T> *list) {
+	void Comp(Node<T>* node, Comparable val, function<bool(T, li)> op, vector<T> *list) {
 		if (node == nullptr) {
 			return;
 		}
@@ -96,7 +99,7 @@ class AVLTree { // Arbol balanceado que nos dio lord Canaval, lo unico nuevo por
 			Comp(node->right, val, op, list);
 		}
 	}
-	void Comp2(Node* node, Comparable val, function<bool(T, string)> op, vector<T> *list) {
+	void Comp2(Node<T>* node, Comparable val, function<bool(T, string)> op, vector<T> *list) {
 		if (node == nullptr) {
 			return;
 		}
@@ -112,72 +115,70 @@ class AVLTree { // Arbol balanceado que nos dio lord Canaval, lo unico nuevo por
 			Comp2(node->right, val, op, list);
 		}
 	}
-
-
-	void preorder(Node* node, function<void(T)> proc) {
+	void preorder(Node<T>* node, function<void(T)> proc) {
 		if (node != nullptr) {
 			proc(node->Elem);
 			preorder(node->left, proc);
 			preorder(node->right, proc);
 		}
 	}
-	void postorder(Node* node, function<void(T)> proc) {
+	void postorder(Node<T>* node, function<void(T)> proc) {
 		if (node != nullptr) {
 			postorder(node->left, proc);
 			postorder(node->right, proc);
 			proc(node->Elem);
 		}
 	}
-	void inorder(Node* node, function<void(T)> proc) {
+	void inorder(Node<T>* node, function<void(T)> proc) {
 		if (node != nullptr) {
 			inorder(node->left, proc);
 			proc(node->Elem);
 			inorder(node->right, proc);
 		}
 	}
-	void reversedInorder(Node* node, function<void(T)> proc) {
+	void reversedInorder(Node<T>* node, function<void(T)> proc) {
 		if (node != nullptr) {
 			reversedInorder(node->right, proc);
 			proc(node->Elem);
 			reversedInorder(node->left, proc);
 		}
 	}
-	int Height(Node* node) { return node == nullptr ? -1 : node->h; }
-	int Lenght(Node* node) { return node == nullptr ? 0 : node->n; }
-	void UpdateHeight(Node* node) {
-		int hl = Height(node->left); // altura izquierda
-		int hr = Height(node->right);
+	li Height(Node<T>* node) { return node == nullptr ? -1 : node->h; }
+	li Lenght(Node<T>* node) { return node == nullptr ? 0 : node->n; }
+	void UpdateHeight(Node<T>* node) {
 		node->h = 1 + max(Height(node->left), Height(node->right));
-		node->n = 1 + Height(node->left) + Lenght(node->right);
+		node->n = 1 + Lenght(node->left) + Lenght(node->right);
 	}
-	Node* RotLeft(Node* node) {
-		Node* NodeAux = node->right;
+	Node<T>* RotLeft(Node<T>* node) {
+		Node<T>* NodeAux = node->right;
 		node->right = NodeAux->left;
 		NodeAux->left = node;
 		UpdateHeight(NodeAux->left);
 		UpdateHeight(NodeAux);
 		return NodeAux;
 	}
-	Node* RotRight(Node* node) {
-		Node* NodeAux = node->left;
+	Node<T>* RotRight(Node<T>* node) {
+		Node<T>* NodeAux = node->left;
 		node->left = NodeAux->right;
 		NodeAux->right = node;
 		UpdateHeight(NodeAux->right);
 		UpdateHeight(NodeAux);
 		return NodeAux;
 	}
-	Node* Balance(Node* node) {
-		int hl = Height(node->left);
-		int hr = Height(node->right);
-		if (hl - hr > 1) { // desbalanceado a la derecha
-			if (Height(node->left->right) > Height(node->left->left)) { // izquierdo derecho es amayor, el hijo izquierdo rotarlo a la izquierda
+	Node<T>* Balance(Node<T>* node) {
+		li hl = Height(node->left);
+		li hr = Height(node->right);
+		if (hl - hr > 1) {
+			if (Height(node->left->right) > Height(node->left->left)) {
 				node->left = RotLeft(node->left);
 			}
+			node = RotRight(node);
 		}
 		else if (hl - hr < -1) {
 			if (Height(node->right->left) > Height(node->right->right)) {
 				node->right = RotRight(node->right);
 			}
+			node = RotLeft(node);
 		}
 		else {
 			UpdateHeight(node);
@@ -185,17 +186,14 @@ class AVLTree { // Arbol balanceado que nos dio lord Canaval, lo unico nuevo por
 		return node;
 	}
 public:
-	AVLTree(function<Comparable(T)> Key = [](T a) {return a; }) : root(nullptr), len(0), Key(Key) {}
-	~AVLTree() { destroy(root); }
-	int size() { return len; }
-	void add(T Elem) { root = add(root, Elem); }
-	int Height() { return Height(root); }
-	bool remove(Comparable val) {
-		// Encontrar el nodo a eliminar
-		// usamos dos elementos auxiliares, uno que comiensce en el nodo que queremos eliminar, y otro que apunte al padre del nodo
-		Node* NodeAux = root;
-		Node* Parent;
-		bool Left; // pongo este Left con mayuscula para que se diferencia con el left del struct nodo
+	AVLTree(function<Comparable(T)> Key = [](T a) {return a; }) : root(nullptr), Key(Key) {}
+	~AVLTree() { Destroy(root); }
+	void Add(T Elem) { root = Add(root, Elem); }
+	T Find(Comparable val) { return Find(root, val); }
+	bool Remove(Comparable val) {
+		Node<T>* NodeAux = root;
+		Node<T>* Parent;
+		bool Left;
 		while (NodeAux != nullptr) {
 			if (val == key(NodeAux->Elem)) {
 				break;
@@ -210,35 +208,45 @@ public:
 				NodeAux = NodeAux->right;
 			}
 		}
-		if (NodeAux == nullptr) return false;
-		if (NodeAux->left == nullptr) { // primer escenario, el hijo derecho reemplaza a su padres
-			if (NodeAux == root) root = NodeAux->right; // el hijo derecho sera la nueva raiz
-			else if (Left) Parent->left = NodeAux->right; // si no es la raiz, el hijo derecho es el nuevo hijo izquierdo 
-			else Parent->right = NodeAux->right;
+		if (NodeAux == nullptr) {
+			return false;
+		}
+		if (NodeAux->left == nullptr) {
+			if (NodeAux == root) {
+				root = NodeAux->right;
+			}
+			else if (Left) {
+				Parent->left = NodeAux->right;
+			}
+			else {
+				Parent->right = NodeAux->right;
+			}
 			delete NodeAux;
 		}
 		else {
-			Node* NodeAux2 = NodeAux->left; // con encontramos el mayor elemento que no tiene hijo derecho
+			Node<T>* NodeAux2 = NodeAux->left;
 			while (NodeAux2->right != nullptr) {
-				Parent = NodeAux2; // puntero que apunta al padre de este elemento
+				Parent = NodeAux2;
 				NodeAux2 = NodeAux2->right;
 			}
-			if (NodeAux2 == NodeAux2->left) NodeAux->left = NodeAux2->left; // el hijo izquierdo del aux2 sera el hijo izquierdo del elemento
-			else Parent->right = NodeAux2->left;
+			if (NodeAux2 == NodeAux2->left) {
+				NodeAux->left = NodeAux2->left;
+			}
+			else {
+				Parent->right = NodeAux2->left;
+			}
 			NodeAux->Elem = NodeAux2->Elem;
 			delete NodeAux2;
 		}
 		delete NodeAux;
 		return true;
 	}
-	T find(Comparable val) { return find(root, val); } // buscar que retorna primera coincidencia, falta buscar que retorne lista de coincidencias
-	// Originalmente debia devolver una lista, por problemas lo deje para que retorne un vector, lo podemos cambiar despues
-	vector<T>* findStarWith(Comparable val, function<bool(T, Comparable)> op) { // buscar que retorna primera coincidencia, falta buscar que retorne lista de coincidencias
+	vector<T>* findStarWith(Comparable val, function<bool(T, Comparable)> op) {
 		vector<T> *result = new vector<T>();
 		findStarWith(root, val, op, result);
 		return result;
 	}
-	vector<T>* findContain(Comparable val, function<bool(T, Comparable)> op) { // buscar que contenga un cadena dentro del nombre
+	vector<T>* findContain(Comparable val, function<bool(T, Comparable)> op) {
 		vector<T>* result = new vector<T>();
 		findContain(root, val, op, result);
 		return result;
@@ -253,7 +261,8 @@ public:
 		Comp2(root, val, op, result66);
 		return result66;
 	}
-
+	li Size() { return Lenght(root); }
+	li Height() { return Height(root); }
 	void preorder(function<void(T)> proc) { preorder(root, proc); }
 	void postorder(function<void(T)> proc) { postorder(root, proc); }
 	void inorder(function<void(T)> proc) { inorder(root, proc); }
